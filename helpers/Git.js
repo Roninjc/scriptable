1d0f7bc41a5702a2e0d021b54d29135a0866fb6b
Jesus Castaño Candela <jesus.castanocandela@gmail.com>
2025-11-01 17:12:16 +0100

    Update Git.js to v0.1.0

--- helpers/Git.js
+++ helpers/Git.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: pink; icon-glyph: magic;
 
 module.exports.computeHash = (str) => {
   let hash = 0;
